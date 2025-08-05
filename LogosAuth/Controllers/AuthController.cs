5606a94a6c07f7a8c659ce6bca99032f2d59a517
cliff-gor <cliff.gor.dev@gmail.com>
2025-08-05 04:12:56 +0300

    Add Register endpoint and login endpoint to register a user with hashed password

---
+++ LogosAuth/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using LogosAuth.Entities;
+using LogosAuth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogosAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request) 
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User Not Found");
+            }
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong Password");
+            }
+
+            string token = "sucess";
+
+            return Ok(token);
+
+        }
+    }
+}
